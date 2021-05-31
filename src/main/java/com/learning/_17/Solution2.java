8ccb9bc165fd64ddde07de7e214daa5de01a27ef
ZhangJiChu <1339446165@qq.com>
2021-05-31 08:40:02 +0800

    Solution

--- src/main/java/com/learning/_17/Solution2.java
+++ src/main/java/com/learning/_17/Solution2.java
@@ -2,7 +2,6 @@ package com.learning._17;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -12,19 +11,21 @@ import java.util.List;
  */
 public class Solution2 {
     List<String> res = new ArrayList<>();
-    HashMap<Character, List<String>> map;
+//    HashMap<Character, List<String>> map;
+//
+//    {
+//        map = new HashMap<>();
+//        map.put('2', Arrays.asList("a", "b", "c"));
+//        map.put('3', Arrays.asList("d", "e", "f"));
+//        map.put('4', Arrays.asList("g", "h", "i"));
+//        map.put('5', Arrays.asList("j", "k", "l"));
+//        map.put('6', Arrays.asList("m", "n", "o"));
+//        map.put('7', Arrays.asList("p", "q", "r", "s"));
+//        map.put('8', Arrays.asList("t", "u", "v"));
+//        map.put('9', Arrays.asList("w", "x", "y", "z"));
+//    }
 
-    {
-        map = new HashMap<>();
-        map.put('2', Arrays.asList("a", "b", "c"));
-        map.put('3', Arrays.asList("d", "e", "f"));
-        map.put('4', Arrays.asList("g", "h", "i"));
-        map.put('5', Arrays.asList("j", "k", "l"));
-        map.put('6', Arrays.asList("m", "n", "o"));
-        map.put('7', Arrays.asList("p", "q", "r", "s"));
-        map.put('8', Arrays.asList("t", "u", "v"));
-        map.put('9', Arrays.asList("w", "x", "y", "z"));
-    }
+    String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) return Arrays.asList();
@@ -38,7 +39,7 @@ public class Solution2 {
             return;
         }
 
-        for (String currentChar : map.get(digits.charAt(index))) {
+        for (char currentChar : map[digits.charAt(index) - '0'].toCharArray()) {
             StringBuilder currentCombination = new StringBuilder(combination);
             letterCombinations(digits, index + 1, currentCombination.append(currentChar));
         }
